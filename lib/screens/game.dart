e081b6daa78ea4b9c54fbf86f7cb4ddc45fd19d2
Supasek Dhanabordeephat <Supasekmai@gmail.com>
2023-01-21 14:30:03 +0700

    Setting up and add google font

---
+++ lib/screens/game.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:tic_tac_toe/constants/color.dart';
+
+class GameScreen extends StatefulWidget {
+  const GameScreen({super.key});
+
+  @override
+  State<GameScreen> createState() => _GameScreenState();
+}
+
+class _GameScreenState extends State<GameScreen> {
+  static var customFontWhite = GoogleFonts.coiny(
+    textStyle: TextStyle(color: Colors.white, letterSpacing: 3, fontSize: 28),
+  );
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: MainColor.primaryColor,
+      body: Center(
+        child: Text(
+          "Tic-Tac-Toe Game",
+          style: customFontWhite,
+        ),
+      ),
+    );
+  }
+}
